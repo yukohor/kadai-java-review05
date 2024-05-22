89a046791b2b423204b3ae125867b886deaa77cd
yukohor <ka91001.xxx@yahoo.co.jp>
2024-05-22 20:10:50 +0900

    kadai-java-review02

---
+++ src/kadai/Review02.java
@@ -0,0 +1,20 @@
+package kadai;
+
+public class Review02 {
+
+    public static void main(String[] args) {
+        int num = 1;
+        for (int i = 1; i <= 100; i++) {
+            if (i % 3 == 0  && i % 5 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+
+}
